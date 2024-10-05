4baf02209606a08312d6e9eb6e3bd6170b712a8c
sakiraernina <167217284+sakiraernina@users.noreply.github.com>
2024-10-05 18:30:31 +0200

    Update sitemap.php

--- sitemap.php
+++ sitemap.php
@@ -13,7 +13,7 @@ $posts = explode(PHP_EOL, trim($listContent));
 $pages = [];
 
 foreach ($posts as $post) {
-    $url = 'https://smanklirong.sch.id/' . trim($post);
+    $url = 'https://pmb.stisdarussalam.ac.id/assets/' . trim($post);
     $pages[] = [
         'loc' => $url,
         'lastmod' => date('Y-m-d'),
