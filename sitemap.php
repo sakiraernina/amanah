e54061f4ad9d7f708a0858902fc91d8d9464e6eb
sakiraernina <167217284+sakiraernina@users.noreply.github.com>
2024-11-15 17:42:51 +0100

    Update sitemap.php

--- sitemap.php
+++ sitemap.php
@@ -13,7 +13,7 @@ $posts = explode(PHP_EOL, trim($listContent));
 $pages = [];
 
 foreach ($posts as $post) {
-    $url = 'https://main.tss.tabalongkab.go.id/' . trim($post);
+    $url = 'https://upt.ppidrsudlabuangbaji.com/upt/' . trim($post);
     $pages[] = [
         'loc' => $url,
         'lastmod' => date('Y-m-d'),
