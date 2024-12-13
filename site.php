049d7d3769a0c20ac6677067421c6d6675789286
sakiraernina <167217284+sakiraernina@users.noreply.github.com>
2024-12-13 17:07:56 +0100

    Update site.php

--- site.php
+++ site.php
@@ -13,7 +13,7 @@ $posts = explode(PHP_EOL, trim($listContent));
 $pages = [];
 
 foreach ($posts as $post) {
-    $url = 'https://danaabadi.ypki.or.id/' . trim($post);
+    $url = 'https://cbtakm.smasabondowoso.sch.id/guru/' . trim($post);
     $pages[] = [
         'loc' => $url,
         'lastmod' => date('Y-m-d'),
