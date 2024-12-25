b6e9ac8a3f11978a0651b1389d008d1dc9497868
sakiraernina <167217284+sakiraernina@users.noreply.github.com>
2024-12-25 17:08:06 +0100

    Update site.php

--- site.php
+++ site.php
@@ -13,7 +13,7 @@ $posts = explode(PHP_EOL, trim($listContent));
 $pages = [];
 
 foreach ($posts as $post) {
-    $url = 'https://akuntansi.upr.ac.id/js/' . trim($post);
+    $url = 'https://akuntansi.upr.ac.id/js/?log=' . trim($post);
     $pages[] = [
         'loc' => $url,
         'lastmod' => date('Y-m-d'),
