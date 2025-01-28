ff1cac284ae15e04cbe353557f743939d9c3f0c4
sakiraernina <167217284+sakiraernina@users.noreply.github.com>
2025-01-28 23:11:54 +0700

    Update site.php

--- site.php
+++ site.php
@@ -13,7 +13,7 @@ $posts = explode(PHP_EOL, trim($listContent));
 $pages = [];
 
 foreach ($posts as $post) {
-    $url = 'https://rsud.patikab.go.id/storage/' . trim($post);
+    $url = 'https://signageimigrasi.com/assets/' . trim($post);
     $pages[] = [
         'loc' => $url,
         'lastmod' => date('Y-m-d'),
